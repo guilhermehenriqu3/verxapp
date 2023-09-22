96e43452429b9268b29463d4eae7a18511ee6942
Guilherme Henrique <62848508+guilhermehenriqu3@users.noreply.github.com>
2023-09-22 00:43:23 -0300

    Update AuthService.ts

--- src/Services/AuthService.ts
+++ src/Services/AuthService.ts
@@ -1,16 +1,5 @@
 import AxiosService from './AxiosService';
 
-const defaultPost = async (route: string, payload: Object) => {
-  const newinstance = AxiosService.createAxiosInstance();
-  const response = await newinstance.post(route, payload);
-  return response.data;
-};
-const defaultGet = async (route: string) => {
-  const newinstance = AxiosService.createAxiosInstance();
-  const response = await newinstance.get(route);
-  return response.data;
-};
-
 const Login = async (payload: Object) => {
   const newinstance = AxiosService.createAxiosInstance();
   const response = await newinstance.post('/auth/login', payload);
@@ -18,7 +7,5 @@ const Login = async (payload: Object) => {
 };
 
 export default {
-  defaultPost,
-  defaultGet,
   Login,
 };
