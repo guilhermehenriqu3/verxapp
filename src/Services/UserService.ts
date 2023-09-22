2738b70b257ffba4e6098c1a2326c4826e6366f8
Guilherme Henrique <62848508+guilhermehenriqu3@users.noreply.github.com>
2023-09-22 02:51:10 -0300

    keystore. + commands.

--- src/Services/UserService.ts
+++ src/Services/UserService.ts
@@ -2,6 +2,7 @@ import AxiosService from './AxiosService';
 
 const GetUserData = async (id: number) => {
   const newinstance = AxiosService.createAxiosInstance();
+  // poderia ter usado a /users mas essa aqui pega do usuario especifico
   const response = await newinstance.get(`/users/${id}`);
   return response.data;
 };
